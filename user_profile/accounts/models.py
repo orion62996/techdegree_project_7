2d6cf514f8dab81d413237a1807c50a58234f8a9
Eric S. Ryan <eric@ericsryan.com>
2019-02-25 15:51:54 -0800

    Added UserProfile model

--- user_profile/accounts/models.py
+++ user_profile/accounts/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255, blank=True)
+    last_name = models.CharField(max_length=255, blank=True)
+    email = models.EmailField(blank=True)
+    birthday = models.DateField(null=True, blank=True)
+    avatar = models.FilePathField(blank=True)
+    bio = models.TextField(blank=True)
+    location = models.CharField(max_length=255, blank=True)
+    ice_cream_flavor = models.CharField(max_length=255, blank=True)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
