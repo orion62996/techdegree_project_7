86b09cf55ffd42964490b0e30abcd121c1a6052f
Eric S. Ryan <eric@ericsryan.com>
2019-05-14 13:33:44 -0700

    Added photo editing features

--- user_profile/accounts/forms.py
+++ user_profile/accounts/forms.py
@@ -3,7 +3,9 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from django.core import validators
+from django.core.files import File
 from django.utils.translation import gettext, gettext_lazy as _
+from PIL import Image
 
 from . import models
 from . import validators as custom_validators
@@ -75,3 +77,35 @@ class UserProfileUpdateForm(forms.ModelForm):
             'location',
             'ice_cream_flavor',
         )
+
+
+class PhotoForm(forms.ModelForm):
+    x = forms.FloatField(widget=forms.HiddenInput())
+    y = forms.FloatField(widget=forms.HiddenInput())
+    width = forms.FloatField(widget=forms.HiddenInput())
+    height = forms.FloatField(widget=forms.HiddenInput())
+
+    class Meta:
+        model = models.Photo
+        fields = (
+            'file',
+            'x',
+            'y',
+            'width',
+            'height',
+        )
+
+    def save(self):
+        photo = super(PhotoForm, self).save()
+
+        x = self.cleaned_data.get('x')
+        y = self.cleaned_data.get('y')
+        w = self.cleaned_data.get('width')
+        h = self.cleaned_data.get('height')
+
+        image = Image.open(photo.file)
+        cropped_image = image.crop((x, y, w+x, h+y))
+        resized_image = cropped_image.resize((200, 200), Image.ANTIALIAS)
+        resized_image.save(photo.file.path)
+
+        return photo
