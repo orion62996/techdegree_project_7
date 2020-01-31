e61d730275ce7f3c87a7025ad990dc6c152dc60a
Eric S. Ryan <eric@ericsryan.com>
2020-01-30 18:30:42 -0800

    Added AvatarEditForm

--- user_profile/avatar_editor/models.py
+++ user_profile/avatar_editor/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class Avatar(models.Model):
     file = models.ImageField(upload_to='images')
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
+
+# class EditedAvatar(models.Model):
+#     file = models.ImageField(upload_to='images')
+#     uploaded_at = models.DateTimeField(auto_now_add=True)
+#     avatar = models.ForeignKeyField()
--- user_profile/avatar_editor/views.py
+++ user_profile/avatar_editor/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 
+from . import forms
 from . import models
 
 
 def edit_avatar(request, pk):
     avatar = get_object_or_404(models.Avatar, pk=pk)
+    form = forms.AvatarEditForm()
+    if request.method == 'POST':
+        form = forms.AvatarEditForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
     return render(request,
                   'avatar_editor/edit_avatar.html',
-                  {'avatar': avatar})
+                  {'avatar': avatar, 'form': form})
